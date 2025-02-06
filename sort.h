72bf15614edfdfea0d475a12c8a7c7330a3e9907
root <moe_aoun@hotmail.com>
2025-02-06 17:18:48 +0000

    0-bubble_sort.c

---
+++ sort.h
@@ -0,0 +1,11 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+
+void bubble_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+#endif /* SORT_H */
+
